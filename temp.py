feff34649fdd555989b69171fb45dabf4906583c
cedricvhoo <c.m.l.van.hoorickx@tue.nl>
2023-07-11 11:54:26 +0200

    Computation files

---
+++ temp.py
@@ -0,0 +1,12 @@
+def mean_temperature(data):
+    """
+    Get the mean temperature
+
+    Args:
+        data (pandas.DataFrame): A pandas dataframe with air temperature measurements.
+
+    Returns:
+        The mean air temperature (float)
+    """
+    temperatures = data['Air temperature (degC)']
+    return float(sum(temperatures)/len(temperatures))
